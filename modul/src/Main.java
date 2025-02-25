5d9ebf8031f96ffc1f601e772f24b434e5451464
TheShadowDragon18 <150584665+TheShadowDragon18@users.noreply.github.com>
2025-02-25 10:32:37 +0200

    Laborator In Class 2

---
+++ modul/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Aceasta este prima mea aplicatie la MPP!");
+    }
+}
